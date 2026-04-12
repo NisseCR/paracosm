9c2f99c86ea6e7de0e015a2dc9d6cabf659da80f
Nisse <nissehermsen@gmail.com>
2026-04-12 14:10:06 +0200

    Revert "Please fix viewport lol"
    This reverts commit dae3b5e53636db799ce5dad94164244ed82670d2.

--- docs/shared/js/effects.js
+++ docs/shared/js/effects.js
@@ -97,7 +97,7 @@ export function initScrollHint() {
         }
     };
 
-    showTimer = window.setTimeout(show, 0);
+    showTimer = window.setTimeout(show, 500);
 
     window.addEventListener('scroll', onScroll, { passive: true });
     window.addEventListener('touchmove', onScroll, { passive: true });
